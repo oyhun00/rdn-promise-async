9f23ad6549b039789c3474226c23d71be5c4ea8a
obeybook <obeybook@gmail.com>
2020-12-01 21:52:15 +0900

    feat: promise 테스트

--- src/learn/Learn1.js
+++ src/learn/Learn1.js
@@ -1,20 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Learn1 = () => {
+  const [greeting, setGreeting] = useState({
+    hello: 'no data'
+  });
+
+  const _promise = (param) => {
+    return new Promise((resolve, reject) => {
+      axios.get('http://localhost:3002/api')
+        .then((res) => {
+          if(param) {
+            resolve(res);
+          } else {
+            reject('error');
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        })
+    })
+  }
+  
   useEffect(() => {
-    axios.get('http://localhost:3002/api')
+    _promise(true)
       .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
+        const { data } = res;
+        setGreeting({
+          hello: data.greeting,
+        })
+      }, (err) => {
         console.log(err);
       })
-  });
+  }, []);
   
   return (
     <>
-      <div>hi</div>
+      <div>{greeting.hello}</div>
     </>
   );
 }
