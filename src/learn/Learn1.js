bbe0d4feaa64a00d15369cff4a054666c1015f2a
obeybook <obeybook@gmail.com>
2020-12-02 22:01:48 +0900

    test: promise all, race 메소드

--- src/learn/Learn1.js
+++ src/learn/Learn1.js
@@ -6,28 +6,36 @@ const Learn1 = () => {
     hello: 'no data'
   });
 
-  const _promise = (param) => {
+  const _promise = () => {
     return new Promise((resolve, reject) => {
-      axios.get('http://localhost:3002/apia')
+      axios.get('http://localhost:3002/api')
         .then((res) => {
-          resolve(res);
+          const { data } = res;
+          resolve(1)
         })
         .catch((err) => {
-          reject(err);
+          console.log(err);
+        })
+    })
+  }
+
+  const _promise2 = () => {
+    return new Promise((resolve, reject) => {
+      axios.get('http://localhost:3002/api')
+        .then((res) => {
+          const { data } = res;
+          resolve(2)
+        })
+        .catch((err) => {
+          console.log(err);
         })
     })
   }
   
+  
   useEffect(() => {
-    _promise(true)
-      .then((res) => {
-        const { data } = res;
-        setGreeting({
-          hello: data.greeting,
-        })
-      }, (err) => {
-        console.log(err);
-      })
+    // Promise.all([_promise, _promise2]).then(() => console.log("1, 2"));
+    Promise.race([_promise, _promise2]).then(console.log);
   }, []);
   
   return (
